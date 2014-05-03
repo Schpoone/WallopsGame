81e9ab6a9c974fe8013eb0957786413efa70306b
1j4k7 <1j4k7@users.noreply.github.com>
2014-05-03 16:54:59 -0400

    Update Game.java

--- src/Game.java
+++ src/Game.java
@@ -9,17 +9,10 @@ public class Game {
 	private JFrame window;
 	private JPanel top;
 	private JPanel bottom;
+	private JPanel inputSection;
+	private JPanel hud;
 	private String[] commandHistory;
-	private JTextArea com1;
-	private JTextArea com2;
-	private JTextArea com3;
-	private JTextArea com4;
-	private JTextArea com5;
-	private JTextArea com6;
-	private JTextArea com7;
-	private JTextArea com8;
-	private JTextArea com9;
-	private JTextArea com10;
+	private JTextArea[] commandPanes;
 	private JTextField textField;
 	private KeyListener commandInput;
 	
@@ -27,17 +20,12 @@ public class Game {
 		window = new JFrame();
 		top = new JPanel();
 		bottom = new JPanel();
-		commandHistory = new String[10];
-		com1 = new JTextArea();
-		com2 = new JTextArea();
-		com3 = new JTextArea();
-		com4 = new JTextArea();
-		com5 = new JTextArea();
-		com6 = new JTextArea();
-		com7 = new JTextArea();
-		com8 = new JTextArea();
-		com9 = new JTextArea();
-		com10 = new JTextArea();
+		inputSection = new JPanel();
+		hud = new JPanel();
+		commandHistory = new String[20];
+		commandPanes = new JTextArea[20];
+		for (int i = 0; i < commandPanes.length; i++)
+			commandPanes[i] = new JTextArea();
 		textField = new JTextField();
 		commandInput = new KeyListener() {
 			
@@ -59,16 +47,10 @@ public class Game {
 				{
 					addCommand(textField.getText());
 					textField.setText("");
-					com10.setText(commandHistory[9]);
-					com9.setText(commandHistory[8]);
-					com8.setText(commandHistory[7]);
-					com7.setText(commandHistory[6]);
-					com6.setText(commandHistory[5]);
-					com5.setText(commandHistory[4]);
-					com4.setText(commandHistory[3]);
-					com3.setText(commandHistory[2]);
-					com2.setText(commandHistory[1]);
-					com1.setText(commandHistory[0]);
+					for (int i = 0; i < commandPanes.length; i++)
+					{
+						commandPanes[i].setText(commandHistory[commandHistory.length - 1 - i]);
+					}
 				}
 				
 			}
@@ -77,32 +59,23 @@ public class Game {
 		window.setExtendedState(JFrame.MAXIMIZED_BOTH);
 		window.setLayout(new GridLayout(2, 1));
 		window.add(top);
-		bottom.setLayout(new GridLayout(11, 1));
-		bottom.add(com10);
-		com10.setEditable(false);
-		bottom.add(com9);
-		com9.setEditable(false);
-		bottom.add(com8);
-		com8.setEditable(false);
-		bottom.add(com7);
-		com7.setEditable(false);
-		bottom.add(com6);
-		com6.setEditable(false);
-		bottom.add(com5);
-		com5.setEditable(false);
-		bottom.add(com4);
-		com4.setEditable(false);
-		bottom.add(com3);
-		com3.setEditable(false);
-		bottom.add(com2);
-		com2.setEditable(false);
-		bottom.add(com1);
-		com1.setEditable(false);
-		bottom.add(textField);
-		window.add(bottom);
-		bottom.setBackground(Color.WHITE);
-		textField.setBackground(Color.GREEN);
 		top.setBackground(Color.BLUE);
+		window.add(bottom);
+		bottom.setLayout(new GridLayout(1, 2));
+		bottom.add(inputSection);
+		bottom.add(hud);
+		hud.setBackground(Color.RED);
+		inputSection.setLayout(new GridLayout(commandPanes.length + 1, 1));
+		Color gray = new Color(170, 170, 170);
+		for (int i = 0; i < commandPanes.length; i++)
+		{
+			inputSection.add(commandPanes[i]);
+			commandPanes[i].setEditable(false);
+			commandPanes[i].setBackground(gray);
+			gray = new Color(gray.getRed() + 3, gray.getGreen() +3, gray.getBlue() + 3);
+		}
+		inputSection.add(textField);
+		textField.setBackground(Color.WHITE);
 		window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		window.setVisible(true);
 	}
