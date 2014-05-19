25a2b093787f435e6402ea8186c6ccc518b587c1
1j4k7 <1j4k7@users.noreply.github.com>
2014-05-19 09:14:52 -0400

    Update Game.java

--- src/Game.java
+++ src/Game.java
@@ -1,136 +1,387 @@
-import java.awt.*;
-import java.awt.event.KeyEvent;
-import java.awt.event.KeyListener;
+import java.awt.Image;
 
-import javax.swing.*;
-
-
-public class Game {
-	private JFrame window;
-	private JPanel view;
-	private JPanel inputSection;
-	private JPanel hud;
-	private ImageIcon health;
-	private JTextArea commandPane;
-	private ScrollPane scroll;
-	private JTextField textField;
-	private KeyListener commandInput;
+public class Wallopsmon {
 	
-	public Game() {
-		
-		initVars();
-		formatVars();
-		addComps();
-		
-		window.setVisible(true);
+	private String name;
+	private String type;
+	private Image mainImage;
+	private Image shinyImage;
+	private boolean isShiny;
+	private int level;
+	private int currentExp;
+	private int expToLevel;
+	private String status;
+	private int maxHealth;
+	private int currentHealth;
+	private int attack;
+	private double attackMult;
+	private int defense;
+	private double defenseMult;
+	private int specAttack;
+	private double specAttackMult;
+	private int specDefense;
+	private double specDefenseMult;
+	private int speed;
+	private double speedMult;
+	private double evasion;
+	private double accuracy;
+	private Item holdItem;
+	private Move move1;
+	private Move move2;
+	private Move move3;
+	private Move move4;
+	private String description;
+	
+	public Wallopsmon() {
+		name = null;
+		type = null;
+		mainImage = null;
+		shinyImage = null;
+		isShiny = false;
+		level = 0;
+		currentExp = 0;
+		expToLevel = 0;
+		status = null;
+		maxHealth = 0;
+		currentHealth = 0;
+		attack = 0;
+		attackMult = 1;
+		defense = 0;
+		defenseMult = 1;
+		specAttack = 0;
+		specAttackMult = 1;
+		specDefense = 0;
+		specDefenseMult = 1;
+		speed = 0;
+		speedMult = 1;
+		evasion = 1;
+		accuracy = 1;
+		holdItem = null;
+		move1 = null;
+		move2 = null;
+		move3 = null;
+		move4 = null;
+		description = null;
 	}
 	
-	public void formatVars() {
-		Rectangle rect = GraphicsEnvironment.getLocalGraphicsEnvironment().getMaximumWindowBounds();
-		window.setSize(new Dimension(rect.width,rect.height));
-		
-		view.setPreferredSize(new Dimension(2*window.getWidth()/3, window.getHeight()));
-		view.setMaximumSize(view.getPreferredSize());
-		view.setMinimumSize(view.getPreferredSize());
-		view.setSize(view.getPreferredSize());
-		
-		textField.setPreferredSize(new Dimension(window.getWidth()/3, window.getHeight()/24));
-		textField.setMaximumSize(textField.getPreferredSize());
-		textField.setSize(textField.getPreferredSize());
-		
-		scroll.setPreferredSize(new Dimension(window.getWidth()/3, 43*window.getHeight()/96));
-		scroll.setMaximumSize(scroll.getPreferredSize());
-		scroll.setSize(scroll.getPreferredSize());
-		
-		inputSection.setPreferredSize(new Dimension(window.getWidth()/3, window.getHeight()/2));
-		inputSection.setMaximumSize(inputSection.getPreferredSize());
-		inputSection.setMinimumSize(inputSection.getPreferredSize());
-		inputSection.setSize(inputSection.getPreferredSize());
-		
-		hud.setPreferredSize(new Dimension(window.getWidth()/3, window.getHeight()/2));
-		hud.setMaximumSize(hud.getPreferredSize());
-		hud.setMinimumSize(hud.getPreferredSize());
-		hud.setSize(hud.getPreferredSize());
-		
-		
-		window.setLayout(new GridBagLayout());
-		hud.setLayout(new GridBagLayout());
-		inputSection.setLayout(new BoxLayout(inputSection, BoxLayout.PAGE_AXIS));
-		
-		view.setBackground(Color.BLUE);
-		hud.setBackground(Color.CYAN);
-		commandPane.setBackground(Color.WHITE);
-		textField.setBackground(Color.PINK);
-		
-		commandPane.setEditable(false);
-		window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	public Wallopsmon(String n, String t, Image main, Image shiny, int l, int maxH, int att, int def, int specAtt, int specDef, int spd, Item hold, Move one, Move two, Move three, Move four, String d) {
+		name = n;
+		type = t;
+		mainImage = main;
+		shinyImage = shiny;
+		if (Math.random()*1000 < 1)
+			isShiny = true;
+		else
+			isShiny = false;
+		level = l;
+		currentExp = (4 * (int)Math.pow(level, 3) / 5);
+		expToLevel = (4 * (int)Math.pow(level + 1, 3) / 5) - currentExp;
+		status = null;
+		maxHealth = maxH;
+		currentHealth = maxH;
+		attack = att;
+		attackMult = 1;
+		defense = def;
+		defenseMult = 1;
+		specAttack = specAtt;
+		specAttackMult = 1;
+		specDefense = specDef;
+		specDefenseMult = 1;
+		speed = spd;
+		speedMult = 1;
+		evasion = 1;
+		accuracy = 1;
+		holdItem = hold;
+		move1 = one;
+		move2 = two;
+		move3 = three;
+		move4 = four;
+		description = d;
 	}
 	
-	public void addComps() {
-		GridBagConstraints c = new GridBagConstraints();
-		
-		c.weightx = 1.0;
-		c.weighty = 1.0;
-		
-		c.fill = GridBagConstraints.BOTH;
-		c.gridwidth = 1;
-		c.gridheight = 2;
-		c.gridx = 0;
-		c.gridy = 0;
-		window.add(view, c);
-		
-		c.gridwidth = 1;
-		c.gridheight = 1;
-		c.gridx = 1;
-		window.add(hud, c);
-		c.gridy = 1;
-		window.add(inputSection, c);
-		
-		textField.addKeyListener(commandInput);
-		
-		scroll.add(commandPane);
-		inputSection.add(scroll);
-		inputSection.add(textField);
-		
-		//hud.add(comp, constraints);
+	public String getName() {
+		return name;
 	}
 	
-	public void initVars() {
-		window = new JFrame();
-		view = new JPanel();
-		inputSection = new JPanel();
-		hud = new JPanel();
-		health = new ImageIcon("health.jpg");
-		commandPane = new JTextArea();
-		textField = new JTextField();
-		scroll = new ScrollPane();
-		commandInput = new KeyListener() {
-			
-			@Override
-			public void keyTyped(KeyEvent e) {}
-			
-			@Override
-			public void keyReleased(KeyEvent e) {
-				if (!textField.getText().isEmpty() && textField.getText().charAt(0) == ' ')
-					textField.setText("");
-			}
-			
-			@Override
-			public void keyPressed(KeyEvent e) {
-				if (e.getKeyCode() == KeyEvent.VK_ENTER && !textField.getText().isEmpty())
-				{
-					addCommand(textField.getText());
-					commandPane.setText(commandPane.getText() + "\n>" + textField.getText());
-					textField.setText("");
-					scroll.setScrollPosition(0, scroll.getVAdjustable().getMaximum()+5);
-				}
-				
-			}
-		};
+	public String getType() {
+		return type;
 	}
 	
-	public void addCommand(String command) {
-		if(command.equals("clear"))
-			commandPane.setText("");
+	public Image getImage() {
+		if (isShiny)
+			return shinyImage;
+		return mainImage;
+	}
+	
+	public boolean isShiny() {
+		return isShiny;
+	}
+	
+	public int getLevel() {
+		return level;
+	}
+	
+	public int getCurrentExp() {
+		return currentExp;
+	}
+	
+	public int getExpToLevel() {
+		return expToLevel;
+	}
+	
+	public String getStatus() {
+		return status;
+	}
+	
+	public int getMaxHealth() {
+		return maxHealth;
+	}
+	
+	public int getCurrentHealth() {
+		return currentHealth;
+	}
+	
+	public int getAttack() {
+		return attack;
+	}
+	
+	public double getAttackMult() {
+		return attackMult;
+	}
+	
+	
+	public int getCurrentAttack() {
+		return (int)(attack * attackMult);
+	}
+	
+	public int getDefense() {
+		return defense;
+	}
+	
+	public double getDefenseMult() {
+		return defenseMult;
+	}
+	
+	public int getCurrentDefense() {
+		return (int)(defense * defenseMult);
+	}
+	
+	public int getSpecAttack() {
+		return specAttack;
+	}
+	
+	public double getSpecAttackMult() {
+		return specAttackMult;
+	}
+	
+	public int getCurrentSpecAttack() {
+		return (int)(specAttack * specAttackMult);
+	}
+	
+	public int getSpecDefense() {
+		return specDefense;
+	}
+	
+	public double getSpecDefenseMult() {
+		return specDefenseMult;
+	}
+	
+	public int getCurrentSpecDefense() {
+		return (int)(specDefense * specDefenseMult);
+	}
+	
+	public int getSpeed() {
+		return speed;
+	}
+	
+	public double getSpeedMult() {
+		return speedMult;
+	}
+	
+	public int getCurrentSpeed() {
+		return (int)(speed * speedMult);
+	}
+	
+	public double getEvasion() {
+		return evasion;
+	}
+	
+	public double getAccuracy() {
+		return accuracy;
+	}
+	
+	public Item getHoldItem() {
+		return holdItem;
+	}
+	
+	public Move getMoveOne() {
+		return move1;
+	}
+	
+	public Move getMoveTwo() {
+		return move2;
+	}
+	
+	public Move getMoveThree() {
+		return move3;
+	}
+	
+	public Move getMoveFour() {
+		return move4;
+	}
+	
+	public String getDescription() {
+		return description;
+	}
+	
+	public void setName(String n) {
+		name = n;
+	}
+	
+	public void setType(String t) {
+		type = t;
+	}
+	
+	public void setMainImage(Image i) {
+		mainImage = i;
+	}
+	
+	public void setShinyImage(Image i) {
+		shinyImage = i;
+	}
+	
+	public void setIsShiny(boolean s) {
+		isShiny = s;
+	}
+	
+	public void setLevel(int l) {
+		level = l;
+	}
+	
+	public void setCurrentExp(int e) {
+		currentExp = e;
+	}
+	
+	public void setExpToLevel(int e) {
+		expToLevel = e;
+	}
+	
+	public void setStatus(String s) {
+		status = s;
+	}
+	
+	public void setMaxHealth(int h) {
+		maxHealth = h;
+	}
+	
+	public void setCurrentHealth(int h) {
+		currentHealth = h;
+	}
+	
+	public void setAttack(int a) {
+		attack = a;
+	}
+	
+	public void setAttackMult(double am) {
+		attackMult = am;
+	}
+	
+	public void setDefense(int d) {
+		defense = d;
+	}
+	
+	public void setDefenseMult(double dm) {
+		defenseMult = dm;
+	}
+	
+	public void setSpecAttack(int sa) {
+		specAttack = sa;
+	}
+	
+	public void setSpecAttackMult(double sam) {
+		specAttackMult = sam;
+	}
+	
+	public void setSpecDefense(int sd) {
+		specDefense = sd;
+	}
+	
+	public void setSpecDefenseMult(double sdm) {
+		specDefenseMult = sdm;
+	}
+	
+	public void setSpeed(int s) {
+		speed = s;
+	}
+	
+	public void setSpeedMult(double sm) {
+		speedMult = sm;
+	}
+	
+	public void setEvasion(double e) {
+		evasion = e;
+	}
+	
+	public void setAccuracy(double a) {
+		accuracy = a;
+	}
+	
+	public void setHoldItem(Item i) {
+		holdItem = i;
+	}
+	
+	public void setMoveOne(Move o) {
+		move1 = o;
+	}
+	
+	public void setMoveTwo(Move t) {
+		move2 = t;
+	}
+	
+	public void setMoveThree(Move t) {
+		move3 = t;
+	}
+	
+	public void setMoveFour(Move f) {
+		move4 = f;
+	}
+	
+	public void setDescription(String d) {
+		description = d;
+	}
+	
+	public void resetInBattleVars() {
+		attackMult = 1;
+		defenseMult = 1;
+		specAttackMult = 1;
+		specDefenseMult = 1;
+		speedMult = 1;
+		evasion = 1;
+		accuracy = 1;
+	}
+	
+	public void fullyHeal() {
+		attackMult = 1;
+		defenseMult = 1;
+		specAttackMult = 1;
+		specDefenseMult = 1;
+		speedMult = 1;
+		evasion = 1;
+		accuracy = 1;
+		status = null;
+		currentHealth = maxHealth;
+	}
+	
+	public void updateExp() {
+		currentExp += calcExpGain();
+		expToLevel -= calcExpGain();
+		if (expToLevel <= 0) {
+			level++;
+			expToLevel = (4 * (int)Math.pow(level + 1, 3) / 5) - currentExp;
+		}
+	}
+	
+	private int calcExpGain() {
+		
 	}
 }
