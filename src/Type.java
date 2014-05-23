e81978b79b276370258b3f5a8ee097b05037b626
1j4k7 <1j4k7@users.noreply.github.com>
2014-05-23 09:26:10 -0400

    Update Type.java

--- src/Type.java
+++ src/Type.java
@@ -6,7 +6,7 @@ public enum Type {
 	ELECTRIC(5, Color.YELLOW), ICE(6, Color.CYAN), DARK(7, Color.BLACK), FIGHTING(8, new Color(200, 35, 15)),
 	FLYING(9, new Color(215, 190, 245)), POISON(10, new Color(185, 10, 230)), GROUND(11, new Color(240, 200, 115)),
 	ROCK(12, new Color(180, 155, 20)), BUG(13, new Color(130, 190, 60)), STEEL(14, Color.GRAY.brighter()),
-	DRAGON(15, new Color(40, 0, 225));
+	DRAGON(15, new Color(90, 10, 200));
 	//this is missing psychic, ghost, and fairy
 	
 	private int value;
