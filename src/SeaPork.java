99f66223608eb0ea97201df4d41d640def602988
1j4k7 <1j4k7@users.noreply.github.com>
2014-05-30 17:07:27 -0400

    Update SeaPork.java

--- src/SeaPork.java
+++ src/SeaPork.java
@@ -1,6 +1,6 @@
 
 public class SeaPork extends Wallopsmon {
 	public SeaPork() {
-		super("SeaPork", Type.WATER, Type.NONE, 1, 25, 5, 15, 5, 15, 5, Move.SPLASH, Move.NONE, Move.NONE, Move.NONE);
+		super("SeaPork", Type.WATER, Type.NONE, 1, 20, 10, 55, 15, 20, 10, Move.SPLASH, Move.NONE, Move.NONE, Move.NONE);
 	}
 }
