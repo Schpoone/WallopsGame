e8f9004c1adf55ae71fd0c2c315e783b050e9f97
1j4k7 <1j4k7@users.noreply.github.com>
2014-05-30 16:14:43 -0400

    Update MudDogWhelk.java

--- src/MudDogWhelk.java
+++ src/MudDogWhelk.java
@@ -1,6 +1,7 @@
 
 public class MudDogWhelk extends Wallopsmon {
 	public MudDogWhelk() {
-		super("MudDogWhelk", Type.WATER, Type.NONE, 1, 50, 10, 10, 10, 10, 10, Move.TACKLE, Move.WITHDRAW, Move.NONE, Move.NONE);
+		super("MudDogWhelk", Type.WATER, Type.NONE, 1, 44, 48, 65, 50, 64, 43, Move.TACKLE, Move.WITHDRAW, Move.NONE, Move.NONE);
+		//base stats are the same as squirtle
 	}
 }
