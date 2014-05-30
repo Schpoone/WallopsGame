dcf7b1d5c875e838c78d8966d6d4d21512b63b36
1j4k7 <1j4k7@users.noreply.github.com>
2014-05-30 17:07:57 -0400

    Update SeaPork.java

--- src/SeaPork.java
+++ src/SeaPork.java
@@ -2,5 +2,6 @@
 public class SeaPork extends Wallopsmon {
 	public SeaPork() {
 		super("SeaPork", Type.WATER, Type.NONE, 1, 20, 10, 55, 15, 20, 10, Move.SPLASH, Move.NONE, Move.NONE, Move.NONE);
+		//same stats as magicarp
 	}
 }
