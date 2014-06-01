ac1c888eec4dee05b4440f1a977e50785f37c634
PureChaose <PureChaose@gmail.com>
2014-06-01 15:10:21 -0400

    now opponent attacks after a switch

--- src/Switch.java
+++ src/Switch.java
@@ -23,6 +23,21 @@ public class Switch implements ActionListener {
 		wallopsmons[loc] = wallopsmons[0];
 		wallopsmons[0] = temp;
 		choose.setVisible(false);
+		if(battle.getOpponent().getCurrentHealth() > 0) {
+			Move m2 = Move.NONE;
+			while(m2.getCategory() != Move.PHYSICAL && m2.getCategory() != Move.SPECIAL) {
+				int n = (int) (Math.random()*3+1);
+				if(n == 1)
+					m2 = battle.getOpponent().getMoveOne();
+				if(n == 2)
+					m2 = battle.getOpponent().getMoveTwo();
+				if(n == 3)
+					m2 = battle.getOpponent().getMoveThree();
+				if(n == 4)
+					m2 = battle.getOpponent().getMoveFour();
+			}
+			battle.attack(m2, battle.getOpponent(), battle.getActive());
+		}
 		battle.update();
 	}
 
