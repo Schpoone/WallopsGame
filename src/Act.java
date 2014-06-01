84805aa3a7dade3a146d2545914255395c577746
PureChaose <PureChaose@gmail.com>
2014-06-01 15:06:54 -0400

    fixed opponent attacking after it faints

--- src/Act.java
+++ src/Act.java
@@ -5,12 +5,12 @@ import java.awt.event.ActionListener;
 public class Act implements ActionListener {
 	private Move btnMv;
 	private Game battle;
-	
+
 	public Act(Move m, Game g) {
 		btnMv = m;
 		battle = g;
 	}
-	
+
 	@Override
 	public void actionPerformed(ActionEvent arg0) {
 		if(btnMv.getCategory() == Move.STATUS_OPPONENT)
@@ -19,19 +19,21 @@ public class Act implements ActionListener {
 			battle.status(btnMv, battle.getActive(), battle.getActive());
 		if(btnMv.getCategory() == Move.PHYSICAL || btnMv.getCategory() == Move.SPECIAL)
 			battle.attack(btnMv, battle.getActive(), battle.getOpponent());
-		Move m2 = Move.NONE;
-		while(m2.getCategory() != Move.PHYSICAL && m2.getCategory() != Move.SPECIAL) {
-			int n = (int) (Math.random()*3+1);
-			if(n == 1)
-				m2 = battle.getOpponent().getMoveOne();
-			if(n == 2)
-				m2 = battle.getOpponent().getMoveTwo();
-			if(n == 3)
-				m2 = battle.getOpponent().getMoveThree();
-			if(n == 4)
-				m2 = battle.getOpponent().getMoveFour();
+		if(battle.getOpponent().getCurrentHealth() > 0) {
+			Move m2 = Move.NONE;
+			while(m2.getCategory() != Move.PHYSICAL && m2.getCategory() != Move.SPECIAL) {
+				int n = (int) (Math.random()*3+1);
+				if(n == 1)
+					m2 = battle.getOpponent().getMoveOne();
+				if(n == 2)
+					m2 = battle.getOpponent().getMoveTwo();
+				if(n == 3)
+					m2 = battle.getOpponent().getMoveThree();
+				if(n == 4)
+					m2 = battle.getOpponent().getMoveFour();
+			}
+			battle.attack(m2, battle.getOpponent(), battle.getActive());
 		}
-		battle.attack(m2, battle.getOpponent(), battle.getActive());
 	}
 
 }
