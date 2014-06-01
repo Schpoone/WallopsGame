b52ce32b281a397c820195f1f512344cf4bef7b5
1j4k7 <1j4k7@users.noreply.github.com>
2014-05-31 20:11:36 -0400

    Create UseItem.java

---
+++ src/UseItem.java
@@ -0,0 +1,27 @@
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.JButton;
+
+public class UseItem implements ActionListener {
+	
+	private JButton item;
+	
+	public UseItem(JButton i) {
+		item = i;
+	}
+	
+	@Override
+	public void actionPerformed(ActionEvent arg0) {
+		if(item.getText().equals("Use Item")) {
+			item.setText("to be implemented later");
+			item.setVisible(false);
+			item.setVisible(true);
+		} else {
+			item.setText("Use Item");
+			item.setVisible(false);
+			item.setVisible(true);
+		}
+	}
+
+}
