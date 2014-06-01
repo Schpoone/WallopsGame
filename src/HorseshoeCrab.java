68bddf2637f34ffc3107b58d09ab52b1c25bc42d
1j4k7 <1j4k7@users.noreply.github.com>
2014-05-31 21:02:06 -0400

    Update HorseshoeCrab.java

--- src/HorseshoeCrab.java
+++ src/HorseshoeCrab.java
@@ -1,7 +1,7 @@
 
 public class HorseshoeCrab extends Wallopsmon {
 	public HorseshoeCrab() {
-		super("HorseshoeCrab", Type.WATER, Type.GROUND, 1, 40, 65, 100, 45, 25, 40, Move.TACKLE, Move.WITHDRAW, Move.WATER_GUN, Move.NONE);
+		super("Horseshoe_Crab", Type.WATER, Type.GROUND, 1, 40, 65, 100, 45, 25, 40, Move.TACKLE, Move.WITHDRAW, Move.WATER_GUN, Move.NONE);
 		//base stats same as shellder
 	}
 
