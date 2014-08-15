388741cff4cd191cf0b224a4543417414d473d4a
1j4k7 <1j4k7@users.noreply.github.com>
2014-08-15 13:59:46 -0400

    Create Beedrill.java

---
+++ src/com/wallops/java/wallopsmon/Beedrill.java
@@ -0,0 +1,10 @@
+package com.wallops.java.wallopsmon;
+
+import com.wallops.java.reference.Move;
+import com.wallops.java.reference.Type;
+
+public class Beedrill extends Wallopsmon{
+	public Beedrill() {
+		super("Beedrill", Type.BUG, Type.POISON, 1, 65, 90, 40, 45, 80, 75, Move.FURY_ATTACK, Move.NONE, Move.NONE, Move.NONE);
+	}
+}
