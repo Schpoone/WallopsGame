e4008f397a388faac3fa6e56c38caddd3fd44904
1j4k7 <1j4k7@users.noreply.github.com>
2014-08-15 16:20:36 -0400

    Create Jigglypuff.java

---
+++ src/com/wallops/java/wallopsmon/Jigglypuff.java
@@ -0,0 +1,10 @@
+package com.wallops.java.wallopsmon;
+
+import com.wallops.java.reference.Move;
+import com.wallops.java.reference.Type;
+
+public class Jigglypuff extends Wallopsmon {
+	public Jigglypuff() {
+		super("Jigglypuff", Type.NORMAL, Type.FAIRY, 1, 115, 45, 20, 45, 25, 20, Move.SING, Move.NONE, Move.NONE, Move.NONE);
+	}
+}
