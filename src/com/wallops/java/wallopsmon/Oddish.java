4715b412ba561f73bafecb0f96c883a146de8b3f
1j4k7 <1j4k7@users.noreply.github.com>
2014-08-15 16:42:58 -0400

    Create Oddish.java

---
+++ src/com/wallops/java/wallopsmon/Oddish.java
@@ -0,0 +1,10 @@
+package com.wallops.java.wallopsmon;
+
+import com.wallops.java.reference.Move;
+import com.wallops.java.reference.Type;
+
+public class Oddish extends Wallopsmon {
+	public Oddish() {
+		super ("Oddish", Type.GRASS, Type.POISON, 1, 45, 50, 55, 75, 65, 30, Move.ABSORB, Move.NONE, Move.NONE, Move.NONE);
+	}
+}
