268dbe524f88713e35382da07be94d4160172870
1j4k7 <1j4k7@users.noreply.github.com>
2014-08-15 14:22:12 -0400

    Create Rattata.java

---
+++ src/com/wallops/java/wallopsmon/Rattata.java
@@ -0,0 +1,10 @@
+package com.wallops.java.wallopsmon;
+
+import com.wallops.java.reference.Move;
+import com.wallops.java.reference.Type;
+
+public class Rattata extends Wallopsmon {
+	public Rattata() {
+		super("Rattata", Type.NORMAL, Type.NONE, 1, 30, 56, 35, 25, 35, 72, Move.TACKLE, Move.TAIL_WHIP, Move.NONE, Move.NONE);
+	}
+}
