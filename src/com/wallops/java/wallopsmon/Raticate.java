921d2b26f0c551d01b9073768be294f858df67fe
1j4k7 <1j4k7@users.noreply.github.com>
2014-08-15 14:27:31 -0400

    Create Raticate.java

---
+++ src/com/wallops/java/wallopsmon/Raticate.java
@@ -0,0 +1,10 @@
+package com.wallops.java.wallopsmon;
+
+import com.wallops.java.reference.Move;
+import com.wallops.java.reference.Type;
+
+public class Raticate extends Wallopsmon {
+	public Raticate() {
+		super("Raticate", Type.NORMAL, Type.NONE, 1, 55, 81, 60, 50, 70, 97, Move.TACKLE, Move.TAIL_WHIP, Move.QUICK_ATTACK, Move.FOCUS_ENERGY);
+	}
+}
