e80e07ed264e18ae25f22bb53ad262b037ba3eba
1j4k7 <1j4k7@users.noreply.github.com>
2014-08-15 15:32:06 -0400

    Create Nidorina.java

---
+++ src/com/wallops/java/wallopsmon/Nidorina.java
@@ -0,0 +1,10 @@
+package com.wallops.java.wallopsmon;
+
+import com.wallops.java.reference.Move;
+import com.wallops.java.reference.Type;
+
+public class Nidorina extends Wallopsmon {
+	public Nidorina() {
+		super("Nidorina", Type.POISON, Type.NONE, 1, 70, 62, 67, 55, 55, 56, Move.GROWL, Move.SCRATCH, Move.NONE, Move.NONE);
+	}
+}
