729a8b5b692a21287a0ee113427204c8c091234c
1j4k7 <1j4k7@users.noreply.github.com>
2014-08-15 14:35:15 -0400

    Create Fearow.java

---
+++ src/com/wallops/java/wallopsmon/Fearow.java
@@ -0,0 +1,10 @@
+package com.wallops.java.wallopsmon;
+
+import com.wallops.java.reference.Move;
+import com.wallops.java.reference.Type;
+
+public class Fearow extends Wallopsmon {
+	public Fearow() {
+		super("Fearow", Type.NORMAL, Type.FLYING, 1, 65, 90, 65, 61, 61, 100, Move.PECK, Move.GROWL, Move.LEER, Move.FURY_ATTACK);
+	}
+}
