04064a176eced971eebdb95ef81c56eca8946c1b
1j4k7 <1j4k7@users.noreply.github.com>
2014-08-15 16:36:43 -0400

    Create Golbat.java

---
+++ src/com/wallops/java/wallopsmon/Golbat.java
@@ -0,0 +1,10 @@
+package com.wallops.java.wallopsmon;
+
+import com.wallops.java.reference.Move;
+import com.wallops.java.reference.Type;
+
+public class Golbat extends Wallopsmon {
+	public Golbat() {
+		super("Golbat", Type.POISON, Type.FLYING, 1, 75, 80, 70, 65, 75, 90, Move.SCREECH, Move.LEECH_LIFE, Move.SUPERSONIC, Move.ASTONISH);
+	}
+}
