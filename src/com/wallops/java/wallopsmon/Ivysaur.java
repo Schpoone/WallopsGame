fec1b7b97612f7dd8e51daca9df8748a2579573d
1j4k7 <1j4k7@users.noreply.github.com>
2014-08-14 21:17:04 -0400

    Update Ivysaur.java

--- src/com/wallops/java/wallopsmon/Ivysaur.java
+++ src/com/wallops/java/wallopsmon/Ivysaur.java
@@ -5,6 +5,6 @@ import com.wallops.java.reference.Type;
 
 public class Ivysaur extends Wallopsmon {
 	public Ivysaur() {
-		super("Ivysaur", Type.GRASS, Type.POISON, 1, 60, 62, 63, 80, 80, 60, /*Item,*/ Move.TACKLE, Move.GROWL, Move.LEECH_SEED, Move.NONE);
+		super("Ivysaur", Type.GRASS, Type.POISON, 1, 60, 62, 63, 80, 80, 60, Move.TACKLE, Move.GROWL, Move.LEECH_SEED, Move.NONE);
 	}
 }
