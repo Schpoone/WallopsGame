a909cccf1448803cab240f9a9b98a3bdafbf76db
1j4k7 <1j4k7@users.noreply.github.com>
2014-08-14 21:28:21 -0400

    Create Charizard.java

---
+++ src/com/wallops/java/wallopsmon/Charizard.java
@@ -0,0 +1,10 @@
+package com.wallops.java.wallopsmon;
+
+import com.wallops.java.reference.Move;
+import com.wallops.java.reference.Type;
+
+public class Charizard extends Wallopsmon {
+	public Charizard() {
+		super("Charizard", Type.FIRE, Type.FLYING, 1, 78, 84, 78, 109, 85, 100, Move.SCRATCH, Move.GROWL, Move.EMBER, Move.SMOKESCREEN);
+	}
+}
