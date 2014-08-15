a28aeea9ed266812edfe81105a26c33a45ff90aa
1j4k7 <1j4k7@users.noreply.github.com>
2014-08-15 16:27:54 -0400

    Create Wigglytuff.java

---
+++ src/com/wallops/java/wallopsmon/Wigglytuff.java
@@ -0,0 +1,10 @@
+package com.wallops.java.wallopsmon;
+
+import com.wallops.java.reference.Move;
+import com.wallops.java.reference.Type;
+
+public class Wigglytuff extends Wallopsmon {
+	public Wigglytuff() {
+		super("Wigglytuff", Type.NORMAL, Type.FAIRY, 1, 140, 70, 45, 85, 50, 45, Move.SING, Move.DISABLE, Move.DEFENSE_CURL, Move.DOUBLE_SLAP);
+	}
+}
