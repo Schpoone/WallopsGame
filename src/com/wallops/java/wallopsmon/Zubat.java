c009679b17a31401d500e5d21ff59e2fb0d3b79f
1j4k7 <1j4k7@users.noreply.github.com>
2014-08-15 16:29:48 -0400

    Create Zubat.java

---
+++ src/com/wallops/java/wallopsmon/Zubat.java
@@ -0,0 +1,10 @@
+package com.wallops.java.wallopsmon;
+
+import com.wallops.java.reference.Move;
+import com.wallops.java.reference.Type;
+
+public class Zubat extends Wallopsmon {
+	public Zubat() {
+		super("Zubat", Type.POISON, Type.FLYING, 1, 40, 45, 35, 30, 40, 55, Move.LEECH_LIFE, Move.NONE, Move.NONE, Move.NONE);
+	}
+}
