035acb13a08d6a57684b20bac80337d720069e2e
1j4k7 <1j4k7@users.noreply.github.com>
2014-08-15 15:30:02 -0400

    Create NidoranFemale.java

---
+++ src/com/wallops/java/wallopsmon/NidoranFemale.java
@@ -0,0 +1,10 @@
+package com.wallops.java.wallopsmon;
+
+import com.wallops.java.reference.Move;
+import com.wallops.java.reference.Type;
+
+public class NidoranFemale extends Wallopsmon {
+	public NidoranFemale() {
+		super("Nidoran♀", Type.POISON, Type.NONE, 1, 55, 47, 52, 40, 40, 41, Move.GROWL, Move.SCRATCH, Move.NONE, Move.NONE);;
+	}
+}
