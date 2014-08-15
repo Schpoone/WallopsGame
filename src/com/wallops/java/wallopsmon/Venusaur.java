544f821682e0da1c2a498ff9c04adbba3ce57ca2
1j4k7 <1j4k7@users.noreply.github.com>
2014-08-14 20:53:25 -0400

    Create Venusaur.java

---
+++ src/com/wallops/java/wallopsmon/Venusaur.java
@@ -0,0 +1,10 @@
+package com.wallops.java.wallopsmon;
+
+import com.wallops.java.reference.Move;
+import com.wallops.java.reference.Type;
+
+public class Venusaur extends Wallopsmon {
+	public Venusaur() {
+		super("Venusaur", Type.GRASS, Type.POISON, 1, 80, 82, 83, 100, 100, 80, Move.TACKLE, Move.GROWL, Move.LEECH_SEED, Move.VINE_WHIP);
+	}
+}
