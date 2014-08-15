6f04476b9ec1f6ce7fc278578e9d29030a54a712
1j4k7 <1j4k7@users.noreply.github.com>
2014-08-14 21:18:06 -0400

    Create Charmeleon.java

---
+++ src/com/wallops/java/wallopsmon/Charmeleon.java
@@ -0,0 +1,10 @@
+package com.wallops.java.wallopsmon;
+
+import com.wallops.java.reference.Move;
+import com.wallops.java.reference.Type;
+
+public class Charmeleon extends Wallopsmon{
+	public Charmeleon() {
+		super("Charmeleon", Type.FIRE, Type.NONE, 1, 58, 64, 58, 80, 65, 80, Move.SCRATCH, Move.GROWL, Move.EMBER, Move.NONE);
+	}
+}
