2ebf6db0111530901b5d5ab47b9028147c909271
1j4k7 <1j4k7@users.noreply.github.com>
2014-08-14 21:44:42 -0400

    Create Blastoise.java

---
+++ src/com/wallops/java/wallopsmon/Blastoise.java
@@ -0,0 +1,10 @@
+package com.wallops.java.wallopsmon;
+
+import com.wallops.java.reference.Move;
+import com.wallops.java.reference.Type;
+
+public class Blastoise extends Wallopsmon {
+	public Blastoise() {
+		super("Blastoise", Type.WATER, Type.NONE, 1, 79, 83, 100, 85, 105, 78, Move.TACKLE, Move.TAIL_WHIP, Move.WATER_GUN, Move.WITHDRAW);
+	}
+}
