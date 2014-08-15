4efc6e4b8c10e7b27ae2204ac4c06a86a2ddb4d4
1j4k7 <1j4k7@users.noreply.github.com>
2014-08-15 16:57:11 -0400

    Create Vileplume.java

---
+++ src/com/wallops/java/wallopsmon/Vileplume.java
@@ -0,0 +1,10 @@
+package com.wallops.java.wallopsmon;
+
+import com.wallops.java.reference.Move;
+import com.wallops.java.reference.Type;
+
+public class Vileplume extends Wallopsmon {
+	public Vileplume() {
+		super("Vileplume", Type.GRASS, Type.POISON, 1, 75, 80, 85, 110, 90, 50, Move.MEGA_DRAIN, Move.AROMATHERAPY, Move.STUN_SPORE, Move.POISON_POWDER);
+	}
+}
