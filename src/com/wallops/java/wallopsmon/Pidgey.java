61003b23d041c2045dac5e5617be2df697e4d90f
1j4k7 <1j4k7@users.noreply.github.com>
2014-08-15 14:02:52 -0400

    Create Pidgey.java

---
+++ src/com/wallops/java/wallopsmon/Pidgey.java
@@ -0,0 +1,10 @@
+package com.wallops.java.wallopsmon;
+
+import com.wallops.java.reference.Type;
+import com.wallops.java.reference.Move;
+
+public class Pidgey extends Wallopsmon {
+	public Pidgey() {
+		super("Pidgey", Type.NORMAL, Type.FLYING, 1, 40, 45, 40, 35, 35, 56, Move.TACKLE, Move.NONE, Move.NONE, Move.NONE);
+	}
+}
