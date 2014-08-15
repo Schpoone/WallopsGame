525bd68d1270232dac497df9fc7e562656b16ba7
1j4k7 <1j4k7@users.noreply.github.com>
2014-08-15 15:55:07 -0400

    Create Clefairy.java

---
+++ src/com/wallops/java/wallopsmon/Clefairy.java
@@ -0,0 +1,10 @@
+package com.wallops.java.wallopsmon;
+
+import com.wallops.java.reference.Move;
+import com.wallops.java.reference.Type;
+
+public class Clefairy extends Wallopsmon {
+	public Clefairy() {
+		super("Clefairy", Type.FAIRY, Type.NONE, 1, 70, 45, 48, 60, 65, 35, Move.POUND, Move.GROWL, Move.ENCORE, Move.DISARMING_VOICE);
+	}
+}
