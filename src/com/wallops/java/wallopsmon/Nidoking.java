1fc3fa349a425fa60d094aef99e00b4363cc2110
1j4k7 <1j4k7@users.noreply.github.com>
2014-08-15 15:44:28 -0400

    Create Nidoking.java

---
+++ src/com/wallops/java/wallopsmon/Nidoking.java
@@ -0,0 +1,10 @@
+package com.wallops.java.wallopsmon;
+
+import com.wallops.java.reference.Move;
+import com.wallops.java.reference.Type;
+
+public class Nidoking extends Wallopsmon {
+	public Nidoking() {
+		super("Nidoking", Type.POISON, Type.GROUND, 1, 81, 102, 77, 85, 75, 85, Move.PECK, Move.FOCUS_ENERGY, Move.DOUBLE_KICK, Move.POISON_STING);
+	}
+}
