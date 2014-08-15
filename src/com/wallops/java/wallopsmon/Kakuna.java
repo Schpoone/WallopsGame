eb92680620f32f23426be46f2e6f2dcea649631d
1j4k7 <1j4k7@users.noreply.github.com>
2014-08-15 13:54:01 -0400

    Create Kakuna.java

---
+++ src/com/wallops/java/wallopsmon/Kakuna.java
@@ -0,0 +1,10 @@
+package com.wallops.java.wallopsmon;
+
+import com.wallops.java.reference.Move;
+import com.wallops.java.reference.Type;
+
+public class Kakuna extends Wallopsmon {
+	public Kakuna() {
+		super("Kakuna", Type.BUG, Type.POISON, 1, 45, 25, 50, 25, 25, 35, Move.HARDEN, Move.NONE, Move.NONE, Move.NONE);
+	}
+}
