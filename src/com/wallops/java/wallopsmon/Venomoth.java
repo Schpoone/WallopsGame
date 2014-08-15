c7dff926647062d902f7e01cb1328a06dad570cf
1j4k7 <1j4k7@users.noreply.github.com>
2014-08-15 17:12:27 -0400

    Create Venomoth.java

---
+++ src/com/wallops/java/wallopsmon/Venomoth.java
@@ -0,0 +1,10 @@
+package com.wallops.java.wallopsmon;
+
+import com.wallops.java.reference.Move;
+import com.wallops.java.reference.Type;
+
+public class Venomoth extends Wallopsmon {
+	public Venomoth() {
+		super("Venomoth", Type.BUG, Type.POISON, 1, 70, 65, 60, 90, 75, 90, Move.TACKLE, Move.DISABLE, Move.FORESIGHT, Move.SUPERSONIC);
+	}
+}
