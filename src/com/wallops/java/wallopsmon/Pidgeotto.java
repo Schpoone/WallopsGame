1a1a9c18bbb99b9d9d805145a87fb15f0bf328dd
1j4k7 <1j4k7@users.noreply.github.com>
2014-08-15 14:11:01 -0400

    Create Pidgeotto.java

---
+++ src/com/wallops/java/wallopsmon/Pidgeotto.java
@@ -0,0 +1,10 @@
+package com.wallops.java.wallopsmon;
+
+import com.wallops.java.reference.Type;
+import com.wallops.java.reference.Move;
+
+public class Pidgeotto extends Wallopsmon {
+	public Pidgeotto() {
+		super("Pidgeotto", Type.NORMAL, Type.FLYING, 1, 63, 60, 55, 50, 50, 71, Move.TACKLE, Move.SAND_ATTACK, Move.GUST, Move.NONE);
+	}
+}
