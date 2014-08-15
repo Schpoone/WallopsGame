f4689cceff4fe5e837379134360669f0cd5a6a49
1j4k7 <1j4k7@users.noreply.github.com>
2014-08-15 17:03:22 -0400

    Create Paras.java

---
+++ src/com/wallops/java/wallopsmon/Paras.java
@@ -0,0 +1,10 @@
+package com.wallops.java.wallopsmon;
+
+import com.wallops.java.reference.Move;
+import com.wallops.java.reference.Type;
+
+public class Paras extends Wallopsmon {
+	public Paras() {
+		super("Paras", Type.BUG, Type.GRASS, 1, 35, 70, 55, 45, 55, 25, Move.SCRATCH, Move.NONE, Move.NONE, Move.NONE);
+	}
+}
