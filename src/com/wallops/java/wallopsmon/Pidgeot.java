d7cf57afd2fe85ef6016943d7e836ded73f8b7f7
1j4k7 <1j4k7@users.noreply.github.com>
2014-08-15 14:19:41 -0400

    Update Pidgeot.java

--- src/com/wallops/java/wallopsmon/Pidgeot.java
+++ src/com/wallops/java/wallopsmon/Pidgeot.java
@@ -1,1 +1,10 @@
-f
+package com.wallops.java.wallopsmon;
+
+import com.wallops.java.reference.Move;
+import com.wallops.java.reference.Type;
+
+public class Pidgeot extends Wallopsmon {
+	public Pidgeot() {
+		super("Pidgeot", Type.NORMAL, Type.FLYING, 1, 83, 80, 75, 70, 70, 101, Move.TACKLE, Move.SAND_ATTACK, Move.GUST, Move.QUICK_ATTACK);
+	}
+}
