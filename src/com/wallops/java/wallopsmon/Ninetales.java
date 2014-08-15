8005fd8d7ab75fd4ca72911dcadf9dd9924f25de
1j4k7 <1j4k7@users.noreply.github.com>
2014-08-15 16:18:00 -0400

    Create Ninetales.java

---
+++ src/com/wallops/java/wallopsmon/Ninetales.java
@@ -0,0 +1,10 @@
+package com.wallops.java.wallopsmon;
+
+import com.wallops.java.reference.Move;
+import com.wallops.java.reference.Type;
+
+public class Ninetales extends Wallopsmon {
+	public Ninetales() {
+		super("Ninetales", Type.FIRE, Type.NONE, 1, 73, 76, 75, 81, 100, 100, Move.FLAMETHROWER, Move.QUICK_ATTACK, Move.CONFUSE_RAY, Move.SAFEGUARD);
+	}
+}
