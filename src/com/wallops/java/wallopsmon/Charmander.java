13b44d66afd28788f86b390ee966cfad3adbd695
1j4k7 <1j4k7@users.noreply.github.com>
2014-08-14 21:14:06 -0400

    Create Charmander.java

---
+++ src/com/wallops/java/wallopsmon/Charmander.java
@@ -0,0 +1,10 @@
+package com.wallops.java.wallopsmon;
+
+import com.wallops.java.reference.Move;
+import com.wallops.java.reference.Type;
+
+public class Charmander extends Wallopsmon {
+	public Charmander() {
+		super("Charmander", Type.FIRE, Type.NONE, 1, 39, 52, 43, 60, 50, 65, Move.SCRATCH, Move.GROWL, Move.NONE, Move.NONE);
+	}
+}
