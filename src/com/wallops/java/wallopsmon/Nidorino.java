bb4ff4544f890fd9bbbf588c68f7c8068823d056
1j4k7 <1j4k7@users.noreply.github.com>
2014-08-15 15:41:51 -0400

    Create Nidorino.java

---
+++ src/com/wallops/java/wallopsmon/Nidorino.java
@@ -0,0 +1,10 @@
+package com.wallops.java.wallopsmon;
+
+import com.wallops.java.reference.Move;
+import com.wallops.java.reference.Type;
+
+public class Nidorino extends Wallopsmon {
+	public Nidorino() {
+		super("Nidorino", Type.POISON, Type.NONE, 1, 61, 72, 57, 55, 55, 65, Move.LEER, Move.PECK, Move.NONE, Move.NONE);
+	}
+}
