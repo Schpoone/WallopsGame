1ea5c156235b4c0ae92924cf5c2073e9676d4db5
1j4k7 <1j4k7@users.noreply.github.com>
2014-10-01 10:15:11 -0400

    Update TableFlipException.java

--- src/com/wallops/java/TableFlipException.java
+++ src/com/wallops/java/TableFlipException.java
@@ -1,7 +1,11 @@
 package com.wallops.java;
 
 import java.util.ArrayList;
+import javax.swing.JFrame;
 
 public class TableFlipException extends Exception {
-	ArrayList<Frame> destroy = new ArrayList<Frame>(10);
+	ArrayList<JFrame> destroy = new ArrayList<JFrame>(10);
+	while (true) {
+		destroy.add(new JFrame());
+	}
 }
