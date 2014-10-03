63aad7fb84ab15954c563b212070c1363e25212a
1j4k7 <1j4k7@users.noreply.github.com>
2014-10-03 10:45:44 -0400

    Update TableFlipException.java

--- src/com/wallops/java/TableFlipException.java
+++ src/com/wallops/java/TableFlipException.java
@@ -3,7 +3,7 @@ package com.wallops.java;
 import java.util.ArrayList;
 import javax.swing.JFrame;
 
-public class TableFlipException extends Exception {
+public class TableFlipException extends RuntimeException {
 	/*
 	 * why am i even fixing this? this isn't even how exceptions work...
 	 */
