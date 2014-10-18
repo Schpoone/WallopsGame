e59c75d2bf3f4d0d59bcb5eee703ca3e84868955
PureChaose <PureChaose@gmail.com>
2014-10-18 00:09:23 -0400

    Move, rename and reimplement TypeEffectiveness Chart as the properties file TypeEffectivenss.

--- src/com/wallops/java/reference/Type.java
+++ src/com/wallops/java/reference/Type.java
@@ -1,34 +1,39 @@
 package com.wallops.java.reference;
 
 import java.awt.Color;
+import java.io.File;
+import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
+import java.io.IOException;
+import java.util.Properties;
 import java.util.Scanner;
 
 
 public enum Type {
 	NONE(-1, Color.WHITE),
+	NORMAL(0, new Color(200, 200, 150)),
 	FIRE(1, Color.RED),
 	WATER(2, Color.BLUE),
-	GRASS(4, Color.GREEN),
-	NORMAL(0, new Color(200, 200, 150)),
 	ELECTRIC(3, Color.YELLOW),
+	GRASS(4, Color.GREEN),
 	ICE(5, Color.CYAN),
-	DARK(15, Color.BLACK),
 	FIGHTING(6, new Color(200, 35, 15)),
-	FLYING(9, new Color(215, 190, 245)),
 	POISON(7, new Color(185, 10, 230)),
 	GROUND(8, new Color(240, 200, 115)),
-	ROCK(12, new Color(180, 155, 20)),
-	BUG(11, new Color(130, 190, 60)),
-	STEEL(16, Color.GRAY.brighter()),
-	DRAGON(14, new Color(90, 10, 200)),
+	FLYING(9, new Color(215, 190, 245)),
 	PSYCHIC(10, new Color(240, 90, 150)),
+	BUG(11, new Color(130, 190, 60)),
+	ROCK(12, new Color(180, 155, 20)),
 	GHOST(13, new Color(120, 85, 150) ),
+	DRAGON(14, new Color(90, 10, 200)),
+	DARK(15, Color.BLACK),
+	STEEL(16, Color.GRAY.brighter()),
 	FAIRY(17, Color.PINK);
 	
 	private int value;
 	private Color color;
+	public static Properties typeAdvantages;
 	
 	private Type(int v, Color c) {
 		value = v;
@@ -57,20 +62,16 @@ public enum Type {
 	}
 	
 	public double getEffectivenessAgainst(Type o) {
-		double[][] chart = new double[18][18];
-		try {
-			Scanner in = new Scanner(new FileReader("src/com/wallops/java/reference/TypeEffectivenessChart.txt"));
-			for (int r = 0; r < chart.length; r++) {
-				for (int c = 0; c < chart[r].length; c++) {
-					chart[r][c] = in.nextDouble();
-				}
+		if(Type.typeAdvantages == null)
+			try {
+				typeAdvantages.load(new FileInputStream(new File("src/com/wallops/resources/TypeEffectiveness.properties")));
+			} catch (FileNotFoundException e1) {
+				// TODO Auto-generated catch block
+				e1.printStackTrace();
+			} catch (IOException e1) {
+				// TODO Auto-generated catch block
+				e1.printStackTrace();
 			}
-		} catch (FileNotFoundException e) {
-			System.out.println("TypeEffectivenessChart.txt not found");
-			System.exit(0);
-		}
-		int att = this.getValue();
-		int def = o.getValue();
-		return chart[att][def];
+		return Double.parseDouble(typeAdvantages.getProperty(this.name()+o.name()));
 	}
 }
