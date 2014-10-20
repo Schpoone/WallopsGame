5baad3ae12ab71682e8e72dd4e4822c34a5a5f76
305027 <305027@757-211C-11-016.mcpsmd.org>
2014-10-20 13:11:02 -0400

    test

--- src/com/wallops/java/TableFlipException.java
+++ src/com/wallops/java/TableFlipException.java
@@ -8,7 +8,7 @@ public class TableFlipException extends RuntimeException {
 	 * why am i even fixing this? this isn't even how exceptions work...
 	 */
 	public TableFlipException() {
-		ArrayList<JFrame> destroy = new ArrayList<JFrame>();
+		ArrayList<JFrame> destroy = new ArrayList<JFrame>();//test
 		int i = 0;
 		while (true) {
 			destroy.add(new JFrame());
