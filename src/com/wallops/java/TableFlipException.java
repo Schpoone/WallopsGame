c6b2de098f5ce2890273a88d71f4018f2ca5aedd
1j4k7 <jasonkuo147@gmail.com>
2014-10-22 13:04:53 -0400

    IT FINALLY WORKS AT SCHOOL!!!!!!!

--- src/com/wallops/java/TableFlipException.java
+++ src/com/wallops/java/TableFlipException.java
@@ -4,9 +4,7 @@ import java.util.ArrayList;
 import javax.swing.JFrame;
 
 public class TableFlipException extends RuntimeException {
-	/*
-	 * why am i even fixing this? this isn't even how exceptions work...
-	 */
+	
 	public TableFlipException() {
 		ArrayList<JFrame> destroy = new ArrayList<JFrame>();
 		int i = 0;
