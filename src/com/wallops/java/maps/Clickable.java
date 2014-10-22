d0b92fdcbefa7c3cd4e3c588ce30c64f699a816f
Jason <jasonkuo147@gmail.com>
2014-10-21 22:40:01 -0400

    some starting interfaces for map tiles

---
+++ src/com/wallops/java/maps/Clickable.java
@@ -0,0 +1,5 @@
+package com.wallops.java.maps;
+
+public interface Clickable {
+	public void whenClickedOn();
+}
---
+++ src/com/wallops/java/maps/Standable.java
@@ -0,0 +1,6 @@
+package com.wallops.java.maps;
+
+public interface Standable {
+	public void whenMovedOnto();
+	public void whenMovedOffOf();
+}
