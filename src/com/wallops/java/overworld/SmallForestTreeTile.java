4fc4c7361fad0662e287bcefe854fbf531665c8d
305027 <305027@10.72.6.11>
2014-10-24 17:51:48 -0400

    SmallForestTreeTile class

---
+++ src/com/wallops/java/overworld/SmallForestTreeTile.java
@@ -0,0 +1,16 @@
+package com.wallops.java.overworld;
+
+public class SmallForestTreeTile extends Tile implements Interactable {
+
+	public SmallForestTreeTile() {
+		super();
+		loadTileTex("smallforesttreetile");
+	}
+
+	@Override
+	public void whenInteractedWith() {
+		// TODO Auto-generated method stub
+		//headbutt
+	}
+
+}
