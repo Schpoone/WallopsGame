b90890308d8134f56737246658250422421f0e1a
305027 <305027@10.72.6.11>
2014-10-27 09:14:47 -0400

    finally all de textures and RedCarpetTile.java

---
+++ src/com/wallops/java/overworld/RedCarpetTile.java
@@ -0,0 +1,21 @@
+package com.wallops.java.overworld;
+
+public class RedCarpetTile extends Tile implements Standable {
+
+	public RedCarpetTile() {
+		super();
+	}
+
+	@Override
+	public void whenMovedOnto() {
+		// TODO Auto-generated method stub
+		//nothing
+	}
+
+	@Override
+	public void whenMovedOffOf() {
+		// TODO Auto-generated method stub
+		//maybe check for map change
+	}
+
+}
