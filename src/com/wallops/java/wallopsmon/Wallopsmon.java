2eca6bcf9b03899896d70ea3270d87a06a538644
305027 <305027@757-330-14-083.mcpsmd.org>
2014-11-11 14:29:38 -0500

    fix my stupidity

--- src/com/wallops/java/wallopsmon/Wallopsmon.java
+++ src/com/wallops/java/wallopsmon/Wallopsmon.java
@@ -95,8 +95,8 @@ public abstract class Wallopsmon {
 		name = n;
 		type1 = t1;
 		type2 = t2;
-		mainImage = Game.textureManager.getTexture(new ResourcePath(ResourcePath.wallopsmonImgDir+"wallopsmon"+File.separator + name + ".jpg"));
-		shinyImage = Game.textureManager.getTexture(new ResourcePath(ResourcePath.wallopsmonImgDir+"wallopsmon"+File.separator +"Shiny" + name + ".jpg"));
+		mainImage = Game.textureManager.getTexture(new ResourcePath(ResourcePath.wallopsmonImgDir+name+".jpg"));
+		shinyImage = Game.textureManager.getTexture(new ResourcePath(ResourcePath.wallopsmonImgDir+"Shiny"+name+".jpg"));
 
 		if (Math.random()*8192 < 1)
 			shiny = true;
