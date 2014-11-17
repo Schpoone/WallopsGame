56af6003f5ce7ce4a96a20219e3f59839136560a
1j4k7 <jasonkuo147@gmail.com>
2014-11-17 00:56:06 -0500

    FUCK YES

--- src/com/wallops/java/TableFlipException.java
+++ src/com/wallops/java/TableFlipException.java
@@ -5,6 +5,6 @@ public class TableFlipException extends RuntimeException {
 	private static final long serialVersionUID = 2352454129112623463L;
 
 	public TableFlipException(Throwable e) {
-		super("Goddamit Jason >.>", e);
+		super("I HATE MY SHIT!", e);
 	}
 }
